189a9352664acfebb7c87b4f9044a5f830acab6f
Matt Watters <mwatt023@uottawa.ca>
2024-02-21 17:11:21 -0500

    commit all

--- Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -10,15 +10,10 @@ import SwiftUI
 struct FrameworkDetailView: View {
     
     var framework: Framework
-    @Binding var isShowingDetailView: Bool
     @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
-            XDismissButton(isShowingDetailView: $isShowingDetailView)
-            
-            Spacer()
-            
             FrameworkTitleView(framework: framework)
             
             Text(framework.description)
@@ -40,5 +35,5 @@ struct FrameworkDetailView: View {
 }
 
 #Preview {
-    FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
+    FrameworkDetailView(framework: MockData.sampleFramework)
 }
